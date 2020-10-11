358188a548d192eba43a1bac7cabe7f1e1aa92f5
Rob Bold <rob@debian2>
2020-10-11 01:04:59 +0100

    Added difficulty levels - 1=easy, 2=medium, 3=hard.

--- src/NumberGuessingGame.js
+++ src/NumberGuessingGame.js
@@ -6,20 +6,31 @@ export class NumberGuessingGame extends React.Component {
   constructor() {
       super();
       this.state = {
+        difficulty: 1,
         target: Math.floor((Math.random() * 10)),
         turnsRemaining: 10,
         guess:0,
         message:""
       }
-      
+      this.setDifficulty = this.setDifficulty.bind(this);
       this.guessChange = this.guessChange.bind(this);
       this.checkGuess = this.checkGuess.bind(this);
       this.newGame = this.newGame.bind(this);
     }
 
+    setDifficulty(event) {
+      var d = event.target.value;
+      if(d >= 1 && d <= 3) {
+        this.setState({
+          difficulty: d,
+        });
+      }
+      this.newGame();
+
+    }
     newGame() {
       this.setState({
-        target: Math.floor(Math.random() * 10),
+        target: Math.floor(Math.random() * Math.pow(10 ,this.state.difficulty)),
         turnsRemaining: 10,
         guess:0,
         message: ""
@@ -55,6 +66,8 @@ export class NumberGuessingGame extends React.Component {
       return(
 
         <div>
+          <label>Difficulty (1, 2, 3)</label>
+          <input type="number" value={this.state.difficulty} onChange={this.setDifficulty} />
           <button onClick={this.newGame}>New Game</button>
           <p>hint: target is {this.state.target}</p>
           <p>{this.state.turnsRemaining} turn(s) remaining</p>
